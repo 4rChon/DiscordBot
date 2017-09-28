b8e78deeda497ab5de0ffffca937398e94393002
Benjamin Bugeja <4rChon@users.noreply.github.com>
2017-09-28 23:53:24 +0200

    Update consts.py

--- bot/consts.py
+++ bot/consts.py
@@ -1,4 +1,4 @@
 """This script defines global constants that are used throughout the bot code."""
 PREFIX = '$'
-CREATOR = 'ArChon#7601'
-TOKEN = 'Mjc2ODM1MzA1ODU0NTk5MTY4.C3VA8g.I6PBVMlYU59kx7jW5GOlWNZFlnA'
+CREATOR = 'DISCORD_NAME#DISCORD_TAG'
+TOKEN = 'YOUR_TOKEN_HERE'
